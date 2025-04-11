19c42e879f565fa891bb0e3486155ea8d2625f37
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-04-11 15:03:06 +0530

    Add StatusBar import and enhance page indicator layout in OnboardingScreen

--- Components/Welcome1.js
+++ Components/Welcome1.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import styles from '../Styles/Welcome1'; 
+import { View, Text, Image, TouchableOpacity, StatusBar } from 'react-native';
+import styles from '../Styles/Welcome1';   
 
 
 export default function OnboardingScreen({ navigation }) {
   return (
     <View style={styles.container}>
+      <View style={styles.pageIndicator}>
+        <View style={[styles.dot, styles.activeDot]} />
+        <View style={[styles.dot, styles. inactiveDot]} />
+        <View style={[styles.dot, styles.inactiveDot]} />
+      </View>
       <Text style={styles.title}>Nurture Your Mind, One Emoji at a Time</Text>
       <Text style={styles.description}>
         Capture your moods and experiences through the language of emojis without any writing. 
@@ -13,7 +18,7 @@ export default function OnboardingScreen({ navigation }) {
       </Text>
 
       <Image
-        source={require('../assets/Welcome1.png')} // Replace with your own asset
+        source={require('../assets/Welcome1.png')} 
         style={styles.image}
         resizeMode="contain"
       />
